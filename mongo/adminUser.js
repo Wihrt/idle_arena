8ac54d833a2fe9eb711202d0a1cdc7ad0b538aa2
Arnaud Hatzenbuhler <arnaud.hatzenbuhler@gmail.com>
2021-08-13 15:45:22 +0200

    Respect Snyk recommandations

--- mongo/adminUser.js
+++
@@ -1,8 +0,0 @@
-db = db.getSiblingDB('admin')
-db.createUser(
-    {
-        user: _getEnv('MONGODB_USER'),
-        pwd: _getEnv('MONGODB_PASSWORD'),
-        roles: [ { role: "readWrite", db: "arena" } ]
-    }
-)
